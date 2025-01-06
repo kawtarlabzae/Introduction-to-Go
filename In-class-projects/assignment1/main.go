bd0bc566f25bf5fd26e3a01cd1bcd7e00c137f04
unknown <kawtar.labzae@um6p.ma>
2025-01-06 17:14:18 +0100

    Part2 assignment1

--- In-class-projects/assignment1/main.go
+++ In-class-projects/assignment1/main.go
@@ -1,72 +1,62 @@
 package main
 
-import("fmt"
-"math/rand/v2")
+import (
+	"fmt"
+	"math/rand/v2"
+)
 
-func main(){
+func main() {
 	fmt.Println("Hello there, please put the limit of the trials")
 	var limitTry int
 	fmt.Scan(&limitTry)
-	randomNumber:=rand.IntN(100)
-	bestScore:=-1
+	randomNumber := rand.IntN(100)
+	bestScore := -1
 	var numGuess int
-	var infinity=true
-	
+	var infinity = true
 
+	fmt.Println("The bestscore now is set to -1")
 
-	for infinity{
-		fmt.Println("Now you can start, please put the number that you guess is right!!")
-		for numGuess=1;numGuess<=limitTry;numGuess++{
-		var guess int
-		fmt.Scan(&guess)
-		if guess<randomNumber{
-			fmt.Println("too low, enter another number")
-			if(numGuess==limitTry){
-				fmt.Println("You lost :(")
+	for infinity {
+		fmt.Println("Now you can start, please put the number that you guess is right !!")
+		for numGuess = 1; numGuess <= limitTry; numGuess++ {
+			var guess int
+			fmt.Scan(&guess)
+			if guess < 0 || guess > 100 {
+				fmt.Println("invalid number")
+				break
 			}
-			continue
-		}else if guess>randomNumber{
-			fmt.Println("too high, enter another number")
-			if(numGuess==limitTry){
-				fmt.Println("You lost :(")
+			if guess < randomNumber {
+				fmt.Println("too low, enter another number")
+				if numGuess == limitTry {
+					fmt.Println("You lost :(")
+				}
+				continue
+			} else if guess > randomNumber {
+				fmt.Println("too high, enter another number")
+				if numGuess == limitTry {
+					fmt.Println("You lost :(")
+				}
+				continue
+			} else {
+				fmt.Printf("You won!! the number is %d with number of attempt %d\n", guess, numGuess)
+				if numGuess < bestScore || bestScore == -1 {
+					bestScore = numGuess
+					fmt.Printf("Your best score now is %d \n", bestScore)
+				} else {
+					fmt.Printf("Your best score was %d \n", bestScore)
+				}
+				numGuess = 1
+				break
 			}
-			continue
-		}else{
-			fmt.Printf("You won!! the number is %d with number of attempt %d\n", guess,numGuess)
-			if numGuess<bestScore || bestScore==-1{
-				bestScore=numGuess
-				fmt.Printf("Your best score now is %d \n",numGuess)
-			}else{fmt.Printf("Your best score was %d \n",bestScore) }
-			numGuess=1
+		}
+		fmt.Println("You can retry if you want, this is done by entering 1 if not please enter anything on the clickboard!!")
+		var a int
+		fmt.Scan(&a)
+		if a != 1 {
+			fmt.Println("It was nice playing with you, goodbye!!")
 			break
 		}
-		}
-	fmt.Println("You can retry if you want, this is done by entering 1 if not please enter anything on the clickboard!!")
-	var a int
-	fmt.Scan(&a)
-	if a != 1{
-		fmt.Println("It was nice playing with you, goodbye!!")
-		break
+		numGuess = 1
 	}
-	numGuess=1
-	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
-
